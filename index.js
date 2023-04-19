3bc9b8df2f3293b006f0abf68523e65a2e360f92
Damon sarceno <Damonsarceno@gmail.com>
2023-04-18 22:01:47 -0400

    slice

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+   cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop(Garfield);
+}
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift(Milo);
+}
+function appendCat(Broom) {
+    return  [...cats, Broom];
+}
+function prependCat(Arnold){
+    return  [Arnold, ...cats];
+}
+function removeLastCat(name) {
+      return cats.slice(0, -1);
+}
+function removeFirstCat(name) {
+     return cats.slice(1);
+}
